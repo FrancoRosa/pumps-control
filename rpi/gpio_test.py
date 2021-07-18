9ed3ed7e5c1c949f88ce145f1e759c5f78f273a8
FrancoRosa <km115.franco@gmail.com>
2021-07-18 01:02:00 +0100

    Test buttons activating relays

--- rpi/gpio_test.py
+++ rpi/gpio_test.py
@@ -1,24 +1,33 @@
 from gpiozero import LED, Button
+from signal import pause
 from time import sleep
 
-led = LED(17)
+pumps = [
+  LED(4),
+  LED(17),
+  LED(27),
+  LED(22)
+]
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+buttons = [
+  Button(18),
+  Button(23),
+  Button(24),
+  Button(12)
+]
 
-"""
-from gpiozero import LED, Button
-from signal import pause
 
-led = LED(17)
-button = Button(3)
+# while True:
+#     for i, pump in enumerate(pumps):
+#       print('... pump:', i)
+#       pump.on()
+#       sleep(.1)
+#       pump.off()
+#       sleep(.2)
 
-button.when_pressed = led.on
-button.when_released = led.off
+
+for i, button in enumerate(buttons):
+    button.when_pressed = pumps[i].on
+    button.when_released = pumps[i].off
 
 pause()
-"""
-
