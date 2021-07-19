b49bbd25a74f441a9b7c3e57459042ba956adbfe
FrancoRosa <km115.franco@gmail.com>
2021-07-18 21:14:29 -0500

    Add pulse timer/counter simulation

--- rpi/control.py
+++ rpi/control.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 from flask_socketio import SocketIO, send
 from flask_cors import CORS
-from time import sleep
+from time import sleep, time
 from threading import Thread
 from os import uname
 import json
@@ -16,10 +16,10 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 port = 9999
 
 pumps_debug = [
-  {'id': 0, 'pulses': 123, 'on': False, 'timeout':10, 'count': 0},
-  {'id': 1, 'pulses': 323, 'on': False, 'timeout':10, 'count': 0},
-  {'id': 2, 'pulses': 123, 'on': True,  'timeout':10, 'count': 0},
-  {'id': 3, 'pulses': 223, 'on': False, 'timeout':10, 'count': 0},
+  {'id': 0, 'total_pulses': 123, 'on': False, 'timeout':10, 'time_count': 0, 'pulses': 0, 'pulses_count':0},
+  {'id': 1, 'total_pulses': 323, 'on': False, 'timeout':10, 'time_count': 0, 'pulses': 0, 'pulses_count':0},
+  {'id': 2, 'total_pulses': 123, 'on': False, 'timeout':10, 'time_count': 0, 'pulses': 0, 'pulses_count':0},
+  {'id': 3, 'total_pulses': 223, 'on': False, 'timeout':10, 'time_count': 0, 'pulses': 0, 'pulses_count':0},
 ]
 # 
 def send_status_rpi():
@@ -29,22 +29,50 @@ def send_status_rpi():
     sleep(5)        
 
 def send_status_debug():
-  
+  global pumps_debug
   while True:
-    t = t+1
-    socketio.send(json.dumps({'t': t}), broadcast=True)
+    for pump in pumps_debug:
+      if pump['on']:
+        pump['total_pulses'] += 1
+        pump['time_count'] += 1
+        pump['pulses_count'] += 1
+      if pump['pulses_count'] >= pump['pulses'] or pump['time_count'] >= pump['timeout']:
+        pump['on'] = False
+        pump['pulses_count'] = 0
+        pump['time_count'] = 0
+      if pump['on']:
+        socketio.send(json.dumps({
+          'id': pump['id'],
+          'pulses_count':pump['pulses_count'],
+          'time_count':pump['time_count'],
+        }), broadcast=True)
     sleep(1)        
 
-
 Thread(target=send_status_rpi if rpi else send_status_debug, args=[]).start()
+
+def start_pump(id, pulses, timeout):
+  global pumps_debug
+  pumps_debug[id]['pulses'] = pulses
+  pumps_debug[id]['timeout'] = timeout
+  pumps_debug[id]['on'] = True
 
 @app.route('/')
 def index():
   return "... control server running on port %s"%port
 
-@app.route('/api/start/<id>')
-def start(id):
-  return "... starting %s pump"%id
+@app.route('/api/start', methods=['post'])
+def start():
+  config = request.get_json()
+  id = int(config['id'])
+  pulses = int(config['pulses'])
+  timeout =  int(config['timeout'])
+  start_pump(id, pulses, timeout)
+  response = make_response(jsonify({
+    "id": id,
+    'total_pulses': pumps_debug[id]['total_pulses']
+  }), 200)
+  response.headers["Content-Type"] = "application/json"
+  return response
 
 @socketio.on('message')
 def handle_message(msg):
