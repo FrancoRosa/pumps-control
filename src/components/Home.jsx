be3d484f1422b7244f32f62eea32d45e2c3b2ee2
FrancoRosa <km115.franco@gmail.com>
2021-07-19 08:30:32 -0500

    Fix refresh issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,29 +6,26 @@ import { useState } from 'react';
 const socket = io.connect('http://localhost:9999');
 
 const Home = () => {
-  var [pumps, setPumps] =  useState([
-      { id: 0, name: 'Pump 1', on: false, timeout: 5, pulses: 10, pulses_count: 0, time_count:0},
-      { id: 1, name: 'Pump 2', on: false, timeout: 8, pulses: 5, pulses_count: 0, time_count:0 },
-      { id: 2, name: 'Pump 3', on: false, timeout: 10, pulses: 7, pulses_count: 0, time_count:0 },
-      { id: 3, name: 'Pump 4', on: false, timeout: 13, pulses: 6, pulses_count: 0, time_count:0 },
-  ])
+  let initState = [
+    { id: 0, name: 'Pump 1', on: false, timeout: 5, pulses: 10, pulses_count: 0, time_count:0},
+    { id: 1, name: 'Pump 2', on: false, timeout: 8, pulses: 5, pulses_count: 0, time_count:0 },
+    { id: 2, name: 'Pump 3', on: false, timeout: 10, pulses: 7, pulses_count: 0, time_count:0 },
+    { id: 3, name: 'Pump 4', on: false, timeout: 13, pulses: 6, pulses_count: 0, time_count:0 },
+  ]
+  let [pumps, setPumps] =  useState(initState)
 
   useEffect(()=>{
-    socket.on('message', msg => {
-      const {id, pulses_count, time_count, on} = JSON.parse(msg);
-      console.log({id, on})
-      let pump = {... pumps[id], pulses_count, time_count, on}
-      let newPumps = [... pumps]
-      newPumps[id] = pump
-      console.log('##################')
-      newPumps.forEach(element => {
-        console.log(element)
-      });
-      setPumps(newPumps)
-    })
+    socket.on('message', msg => updatePumps(msg))
   },[])
 
-  
+  const updatePumps = msg => {
+    let pump = JSON.parse(msg);
+    let id = pump.id
+    pump = {... initState[id], ...pump}
+    initState[id] = pump
+    console.log(pump)
+    setPumps(initState)
+  }
   
   const handleClick = pump => {
     startPump(pump).then(resp => console.log(resp))
