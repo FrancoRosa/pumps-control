f62ba1a8dd65ce6bd3bc338f57db635f23ddad61
FrancoRosa <km115.franco@gmail.com>
2021-08-02 09:51:56 -0500

    Stopwatch implementation

--- src/components/CardManual.jsx
+++ src/components/CardManual.jsx
@@ -1,10 +1,35 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { PumpsContext } from "../js/PumpsContext";
 import Input from "./Input";
 
 const CardManual = ({ pump }) => {
   const {pumps, setPumps} = useContext(PumpsContext)
   const { id } = pump
+  const [seconds, setSeconds] = useState(0)
+  const [running, setRunning] = useState(false)
+  var counter = 0
+
+  const updateStopWatch = () => {
+    counter = counter + 0.500
+    setSeconds(counter.toFixed(1));
+  } 
+
+  
+  const handleTimer = () => {
+    let timer = null
+    
+    if (!running){
+      console.log('start')
+      counter = 0
+      setSeconds(counter)
+      setRunning(true)
+      timer = setInterval(updateStopWatch, 500)
+    } else {
+      console.log('stop')
+      setRunning(false)
+      clearInterval(timer)
+    }
+  }
 
   return (
     <div className="column">
@@ -14,22 +39,17 @@ const CardManual = ({ pump }) => {
             {pump.name}
           </p>
         </header>
+        <p>{running ? 'running' : 'stopped'}</p>
         <p className="has-text-link heading has-text-centered mt-4">Stopwatch</p>
-        <p className="title is-3 success has-text-centered">00:00</p>
+        <p className="title is-3 success has-text-centered">{seconds}</p>
         <p className="has-text-link heading has-text-centered mt-4">Pulses</p>
         <p className="title is-3 success has-text-centered">0</p>
         <div className="card-content is-flex is-justify-content-center m-0 p-0">
           <button
-            className="button m-3"
-            onClick={() => console.log('...start', id)}
+            className="button m-2 is-medium"
+            onClick={handleTimer}
           >
-            Start
-          </button>
-          <button
-            className="button m-3"
-            onClick={() => console.log('...stop', id)}
-          >
-            Stop
+            {running ? 'Stop' : 'Start'}
           </button>
         </div>
       </div>
