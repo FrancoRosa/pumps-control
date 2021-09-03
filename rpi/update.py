88044783888ebb9da7ad028310e3b0fdb34332e5
FrancoRosa <km115.franco@gmail.com>
2021-09-03 17:33:11 -0500

    Add update script

--- rpi/update.py
+++ rpi/update.py
@@ -1,1 +1,9 @@
-import
+from subprocess import check_output
+from os import chdir
+
+PROJECT_DIR = '/home/pi/pumps-control'
+print(chdir(PROJECT_DIR))
+print(check_output('pwd'))
+new_data = check_output(['git', 'fetch'])
+if new_data:
+    check_output(['git', 'pull', 'origin', 'master'])
